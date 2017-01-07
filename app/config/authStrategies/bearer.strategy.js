a9cc2fcd383d59b194492ce450ec80a7758014eb
Manuel Navarro <manuelnavarro1987@gmail.com>
2017-01-07 17:16:49 +0100

    Auth: bearer strategy

---
+++ app/config/authStrategies/bearer.strategy.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const passport = require('passport');
+const Strategy = require('passport-http-bearer').Strategy;
+const User     = require('./../../models').user;
+const secret   = require('./../auth');
+const jwt      = require('jsonwebtoken');
+
+passport.use(new Strategy(function (token, cb) {
+  jwt.verify(token, secret.key, function(err, decoded) {
+    if (err) return cb(err);
+    User.findById(decoded.id, (err, user) => cb(null, user ? user : false));
+  });
+}));
--- server.js
+++ server.js
@@ -1,24 +1,29 @@
 'use strict';
 
 const express    = require('express');
+const passport   = require('passport');
 const mongoose   = require('mongoose');
 const bodyParser = require('body-parser');
 const morgan     = require('morgan');
 
+require('./app/config/authStrategies/local.strategy');
+require('./app/config/authStrategies/bearer.strategy');
+
 const PORT = process.env.PORT || 8080;
 
-// App
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(passport.initialize());
 app.use(morgan('combined'));
 
 mongoose.Promise = global.Promise;
 
 mongoose.connect('mongodb://mongo/api');
 
-app.use("/api", require("./app/routes"));
+app.use("/api", passport.authenticate('bearer', { session: false }), require("./app/routes"));
+app.use("/auth", require("./app/services"));
 
 app.all('/', function (req, res) {
   res.send('Work Parts Project\n');
